1776e683781ce17df814275bc1461836d1284317
Adeyemi Atayero <henry.atayero@gmail.com>
2021-11-30 10:07:34 -0600

    adding typewriter.js to the typewriter repo

---
+++ typewriter.js
@@ -0,0 +1,120 @@
+const sentence = "hello there from lighthouse labs";
+
+// for (const char of sentence) {
+//   setTimeout(() => {
+//     process.stdout.write(char);
+//   }, 1000)
+// }
+
+//console.log(sentence[0]);
+
+setTimeout(() => {
+  process.stdout.write(sentence[0]);
+}, 1100)
+
+setTimeout(() => {
+  process.stdout.write(sentence[1]);
+}, 1150)
+
+setTimeout(() => {
+  process.stdout.write(sentence[2]);
+}, 1200)
+
+setTimeout(() => {
+  process.stdout.write(sentence[3]);
+}, 1250)
+setTimeout(() => {
+  process.stdout.write(sentence[4]);
+}, 1300)
+
+setTimeout(() => {
+  process.stdout.write(sentence[5]);
+}, 1350)
+
+setTimeout(() => {
+  process.stdout.write(sentence[6]);
+}, 1400)
+
+setTimeout(() => {
+  process.stdout.write(sentence[7]);
+}, 1500)
+
+setTimeout(() => {
+  process.stdout.write(sentence[8]);
+}, 1550)
+
+setTimeout(() => {
+  process.stdout.write(sentence[9]);
+}, 1600)
+
+setTimeout(() => {
+  process.stdout.write(sentence[10]);
+}, 1650)
+
+setTimeout(() => {
+  process.stdout.write(sentence[11]);
+}, 1700)
+
+setTimeout(() => {
+  process.stdout.write(sentence[12]);
+}, 1750)
+
+setTimeout(() => {
+  process.stdout.write(sentence[13]);
+}, 1800)
+
+setTimeout(() => {
+  process.stdout.write(sentence[14]);
+}, 1850)
+
+setTimeout(() => {
+  process.stdout.write(sentence[15]);
+}, 1900)
+
+setTimeout(() => {
+  process.stdout.write(sentence[16]);
+}, 2000)
+
+setTimeout(() => {
+  process.stdout.write(sentence[17]);
+}, 2050)
+
+setTimeout(() => {
+  process.stdout.write(sentence[18]);
+}, 2100)
+
+setTimeout(() => {
+  process.stdout.write(sentence[19]);
+}, 2200)
+
+setTimeout(() => {
+  process.stdout.write(sentence[20]);
+}, 2250)
+
+setTimeout(() => {
+  process.stdout.write(sentence[21]);
+}, 2300)
+
+setTimeout(() => {
+  process.stdout.write(sentence[22]);
+}, 2350)
+
+setTimeout(() => {
+  process.stdout.write(sentence[23]);
+}, 2400)
+
+setTimeout(() => {
+  process.stdout.write(sentence[24]);
+}, 2450)
+
+setTimeout(() => {
+  process.stdout.write(sentence[25]);
+}, 2500)
+
+// setTimeout(() => {
+//   process.stdout.write(sentence[26]);
+// }, 2550)
+
+setTimeout(() => {
+  console.log(sentence[26]);
+}, 2600)
